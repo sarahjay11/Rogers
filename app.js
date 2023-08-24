c0675f4dda5b4098c9237a74954c25508bb9a9e9
sarahjay11 <143091688+sarahjay11@users.noreply.github.com>
2023-08-24 05:19:09 -0700

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_gq7an17", "template_xe7frwd", formData)
+    emailjs.send("service_z95g76c", "template_7zuqg1d", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
